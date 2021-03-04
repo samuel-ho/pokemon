ef688bbb78a5800eeb14e9eec5c6e8a3a5e7a583
Samuel Ho <samueldaraho@gmail.com>
2021-03-04 15:09:37 -0800

    Extract store configuration into its own file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 
 import App from './App';
-import reducers from './reducers/index'
+import store from './store';
 import './index.css';
-
-const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}> 
